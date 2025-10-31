c280659625ff9146aa3b166f038eaee1467ce743
Brad Aagaard <baagaard@usgs.gov>
2025-10-31 07:54:12 -0600

    Add OutputTriggers

---
+++ packages/pylith/observers/output_triggers/OutputTriggerStep.py
@@ -0,0 +1,36 @@
+# =================================================================================================
+# This code is part of PyLith, developed through the Computational Infrastructure
+# for Geodynamics (https://github.com/geodynamics/pylith).
+#
+# Copyright (c) 2010-2025, University of California, Davis and the PyLith Development Team.
+# All rights reserved.
+#
+# See https://mit-license.org/ and LICENSE.md and for license information.
+# =================================================================================================
+import pyre
+
+import pylith
+from pylith import journal
+
+from .OutputTrigger import OutputTrigger
+
+
+class OutputTriggerStep(pyre.component, implements=OutputTrigger, family="pylith.observers.output_triggers.step"):
+    """Trigger output based on time step index."""
+
+    num_skip = pylith.properties.int(default=0)
+    num_skip.validators = pyre.constraints.isGreaterEqual(value=0)
+    num_skip.doc = "Number of solution steps to skip between writes (0 means write every time step)."
+
+    def __init__(self, name, locator, implicit, **kwds):
+        """Constructor."""
+        super().__init__(name, locator, implicit, **kwds)
+
+        todo = journal.warning(":TODO:")
+        todo.report(
+            (
+                "Implement OutputTriggerStep.__init__(). Pass parameters to C++.",
+                f"num skip={self.num_skip}",
+            )
+        )
+        todo.log()

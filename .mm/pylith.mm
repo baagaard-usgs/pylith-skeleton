cac323e5df3d9b91106d44b44ee7d88a22398aef
Brad Aagaard <baagaard@usgs.gov>
2026-07-12 13:45:22 -0600

    WIP: Setup mm build for gui.

--- .mm/pylith.mm
+++ .mm/pylith.mm
@@ -5,13 +5,18 @@
 
 # the project assets
 pylith.packages := pylith.pkg spatialdata.pkg
-pylith.tests    := pylith.pytest spatialdata.pytest
+
+# a web bundle
+pylith.webpack := pylith.web
+
+pylith.tests := pylith.pytest spatialdata.pytest
 
 # mpi is a Python runtime dependency (via mpi4py); declared here for documentation
 pylith.extern := mpi
 
 # load configurations
 include $(pylith.packages)
+include $(pylith.webpack)
 include $(pylith.tests)
 
 
